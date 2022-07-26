f169abaeb4dd3dce968d0b302bab22f20f5dd51c
Antonio Iorio <antonioiorio94@gmail.com>
2022-07-26 19:25:24 +0200

    add recipe details page and link to other pages

--- toogood/src/components/Popular.jsx
+++ toogood/src/components/Popular.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Link } from "react-router-dom";
 import "@splidejs/react-splide/css";
 //Funzione per lanciare la funzione appena la pagina ricarica
 function Popular() {
@@ -45,9 +46,11 @@ function Popular() {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
+                  <Link to={"/recipe/" + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Link>
                 </Card>
               </SplideSlide>
             );
